b8b1d42ca50cd505598f17d44ce01e95b388a63b
Yudhistira <yudhistira.tribuana@gmail.com>
2024-05-06 21:01:37 +0700

    Added add activity dialog

--- client/src/components/activityListHeader.tsx
+++ client/src/components/activityListHeader.tsx
@@ -1,7 +1,13 @@
-import { Button } from '@/components/ui/button'
 import Searchbar from '@/components/searchbar'
 import { cn } from '@/lib/utils'
 import { PlusCircle } from 'lucide-react'
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from '@/components/ui/dialog'
 
 interface ActivityListHeaderProps
 	extends Partial<React.HTMLAttributes<HTMLDivElement>> {}
@@ -12,13 +18,18 @@ export default function ActivityListHeader({
 		<div {...props} className={cn('flex justify-between', props.className)}>
 			<div className="flex gap-4 items-center">
 				<p className="font-bold text-xl">Daftar Kegiatan</p>
-				<Button
-					variant="ghost"
-					className="flex gap-2 font-bold text-timesheet-blue bg-timesheet-blue hover:bg-timesheet-blue hover:text-timesheet-blue hover:bg-opacity-5 bg-opacity-10"
-				>
-					<PlusCircle />
-					Tambah Kegiatan
-				</Button>
+				<Dialog>
+					<DialogTrigger className="flex p-1.5 items-center rounded gap-1 text-base font-bold text-timesheet-blue bg-timesheet-blue hover:bg-timesheet-blue hover:text-timesheet-blue hover:bg-opacity-5 bg-opacity-10">
+						<PlusCircle height={20} />
+						Tambah Kegiatan
+					</DialogTrigger>
+					<DialogContent>
+						<DialogHeader>
+							<DialogTitle>Filter</DialogTitle>
+						</DialogHeader>
+						hi
+					</DialogContent>
+				</Dialog>
 			</div>
 			<div className="flex gap-4 items-center">
 				<Searchbar />
