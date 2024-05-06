c027d69b9e400f7d4beedd2e1b48c82018e6cded
Yudhistira <yudhistira.tribuana@gmail.com>
2024-05-06 18:19:22 +0700

    Finished duration calculation

--- client/src/components/activityTable.tsx
+++ client/src/components/activityTable.tsx
@@ -1,4 +1,5 @@
 import { DataTable } from '@/components/ui/dataTable'
+import { extendedDayjs } from '@/lib/utils'
 import { Activity } from '@/models/activity'
 import { ColumnDef } from '@tanstack/react-table'
 const columns: ColumnDef<Activity>[] = [
@@ -13,22 +14,68 @@ const columns: ColumnDef<Activity>[] = [
 	{
 		accessorKey: 'date_start',
 		header: 'Tanggal Mulai',
+		cell: (cell) => (
+			<div>
+				{extendedDayjs(cell.getValue() as string)
+					.locale('id')
+					.format('DD MMM YYYY')}
+			</div>
+		),
 	},
 	{
 		accessorKey: 'date_end',
 		header: 'Tanggal Berakhir',
+		cell: (cell) => (
+			<div>
+				{extendedDayjs(cell.getValue() as string)
+					.locale('id')
+					.format('DD MMM YYYY')}
+			</div>
+		),
 	},
 	{
 		accessorKey: 'time_start',
 		header: 'Waktu Mulai',
+		cell: (cell) => (
+			<div>
+				{extendedDayjs(cell.getValue() as string, 'HH:mm:ss')
+					.locale('id')
+					.format('HH:mm')}
+			</div>
+		),
 	},
 	{
 		accessorKey: 'time_end',
 		header: 'Waktu Berakhir',
+		cell: (cell) => (
+			<div>
+				{extendedDayjs(cell.getValue() as string, 'HH:mm:ss')
+					.locale('id')
+					.format('HH:mm')}
+			</div>
+		),
 	},
 	{
 		header: 'Durasi',
-		cell: () => <div>test</div>,
+		cell: ({ row }) => {
+			const startTime = extendedDayjs(row.original.time_start, 'HH:mm:ss')
+			const start = extendedDayjs(row.original.date_start)
+				.add(startTime.hour(), 'hour')
+				.add(startTime.minute(), 'minute')
+			const endTime = extendedDayjs(row.original.time_end, 'HH:mm:ss')
+			const end = extendedDayjs(row.original.date_end)
+				.add(endTime.hour(), 'hour')
+				.add(endTime.minute(), 'minute')
+			const day = end.diff(start, 'd')
+			const hour = end.diff(start, 'h')
+			const minute = end.diff(start, 'm') % 60
+			return (
+				<div>
+					{day > 0 && `${day} hari`} {hour > 0 && `${hour} jam`}{' '}
+					{minute > 0 && `${minute} menit`}
+				</div>
+			)
+		},
 	},
 	{
 		header: 'Aksi',
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,5 +1,8 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import customParseFormat from "dayjs/plugin/customParseFormat"
+import dayjs from "dayjs"
+import 'dayjs/locale/id'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -10,3 +13,6 @@ export const moneyFormatter = Intl.NumberFormat('id-ID', {
 	style: 'currency',
 	maximumFractionDigits: 0,
 })
+
+dayjs.extend(customParseFormat)
+export const extendedDayjs = dayjs
